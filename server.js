d09d952ed62e068ffd47fa6ab11e2fc661820c8b
luuuizz253 <juanluisromero863@gmail.com>
2026-08-28 16:53:30 -0300

    Add settings routes to the server

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const express = require("express");
 const cors = require("cors");
 const ordersRoutes = require("./routes/orders");
 const paymentsRoutes = require("./routes/payments");
+const settingsRoutes = require("./routes/settings");
 
 const app = express();
 app.use(cors());
 
 app.use("/api/orders", ordersRoutes);
 app.use("/api/payments", paymentsRoutes);
+app.use("/api/settings", settingsRoutes);
 
 app.get("/", (req, res) => res.send("API imprenta funcionando"));
 
